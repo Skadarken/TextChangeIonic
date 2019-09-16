9a99d1b033c9a09c51bf3f390beb30c2bfa54ff5
Skadarken <harishb159@gmail.com>
2019-09-16 13:21:17 +0530

    Text change

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  text = 'Deafult starting text';
+
+  onChangeText() {
+    this.text = 'Changed!';
+  }
 
 }
